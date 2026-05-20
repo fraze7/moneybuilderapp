b8ed7c919677452b90844b4d8261cfdbd181d6da
fraze7 <frazer.fox@gmail.com>
2026-05-20 16:47:21 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', maxWidth: 600, margin: '60px auto', textAlign: 'center' }}>
+      <h1>Money Builder App</h1>
+      <p>Your investment app is running. Add your components here.</p>
+    </div>
+  );
+}
